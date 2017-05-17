5188300e116d57b00b5f09d1921615cd495dcfb9
tundi <olatunde.awoyemi@ovoenergy.com>
2017-05-17 14:52:31 +0100

    model request states

--- src/main/scala/example/TypeTetris.scala
+++ src/main/scala/example/TypeTetris.scala
@@ -1,12 +1,20 @@
 object TypeTetris {
   import scala.concurrent.Future
 
+  sealed trait Request //agebraic data type
+
+  object Request {
+    final case class Unauthorized() extends Request
+    final case class Authorized() extends Request
+  }
+
   // abstract types
-  type Request
   type Response
 
-  def service(request: Request): Future[Response] =
-    (authorize _ andThen ???)(request)
+  def service(request: Request.Unauthorized): Future[Response] = {
+    val authorized = authorize(request)
+    ???
+  }
 
-  def authorize(request: Request): Request = ???
+  def authorize(request: Request.Unauthorized): Option[Request.Authorized] = ???
 }
